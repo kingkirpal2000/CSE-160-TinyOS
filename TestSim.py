dd8f31f11d03f979ffd09153528b39981c706c97
Alex Beltran <abeltran2@ucmerced.edu>
2016-08-25 20:59:08 +0000

    Moved simulation to a class and put it outside of the run file.

---
+++ TestSim.py
@@ -0,0 +1,129 @@
+#ANDES Lab - University of California, Merced
+#Author: UCM ANDES Lab
+#$Author: abeltran2 $
+#$LastChangedDate: 2014-08-31 16:06:26 -0700 (Sun, 31 Aug 2014) $
+#! /usr/bin/python
+import sys
+from TOSSIM import *
+from CommandMsg import *
+
+class TestSim:
+    # COMMAND TYPES
+    CMD_PING = 0
+    CMD_NEIGHBOR_DUMP = 1
+    CMD_ROUTE_DUMP=3
+
+    # Initialize Vars
+    numMote=0
+
+    def __init__(self):
+        self.t = Tossim([])
+        self.r = self.t.radio()
+
+        #Create a Command Packet
+        self.msg = CommandMsg()
+        self.pkt = self.t.newPacket()
+        self.pkt.setType(self.msg.get_amType())
+
+    # Load a topo file and use it.
+    def loadTopo(self, topoFile):
+        print 'Creating Topo!'
+        # Read topology file.
+        topoFile = 'topo/'+topoFile
+        f = open(topoFile, "r")
+        self.numMote = int(f.readline());
+        print 'Number of Motes', self.numMote
+        for line in f:
+            s = line.split()
+            if s:
+                print " ", s[0], " ", s[1], " ", s[2];
+                self.r.add(int(s[0]), int(s[1]), float(s[2]))
+
+    # Load a noise file and apply it.
+    def loadNoise(self, noiseFile):
+        if self.numMote == 0:
+            print "Create a topo first"
+            return;
+
+        # Get and Create a Noise Model
+        noiseFile = 'noise/'+noiseFile;
+        noise = open(noiseFile, "r")
+        for line in noise:
+            str1 = line.strip()
+            if str1:
+                val = int(str1)
+            for i in range(1, self.numMote+1):
+                self.t.getNode(i).addNoiseTraceReading(val)
+
+            for i in range(1, self.numMote+1):
+                print "Creating noise model for ",i;
+                self.t.getNode(i).createNoiseModel()
+
+    def bootNode(self, nodeID):
+        if self.numMote == 0:
+            print "Create a topo first"
+            return;
+        self.t.getNode(nodeID).bootAtTime(1333*nodeID);
+
+    def bootAll(self):
+        i=0;
+        for i in range(1, self.numMote+1):
+            self.bootNode(i);
+
+    def moteOff(self, nodeID):
+        self.t.getNode(nodeID).turnOff();
+
+    def moteOn(self, nodeID):
+        self.t.getNode(nodeID).turnOn();
+
+    def run(self, ticks):
+        for i in range(ticks):
+            self.t.runNextEvent()
+
+    # Rough run time. tickPerSecond does not work.
+    def runTime(self, amount):
+        i=0
+        while i<amount*1000:
+            self.t.runNextEvent()
+            i=i+1
+
+    # Generic Command
+    def sendCMD(self, ID, dest, payloadStr):
+        self.msg.set_dest(dest);
+        self.msg.set_id(ID);
+        self.msg.setString_payload(payloadStr)
+
+        self.pkt.setData(self.msg.data)
+        self.pkt.setDestination(dest)
+        self.pkt.deliver(dest, self.t.time()+5)
+
+    def ping(self, source, dest, msg):
+        self.sendCMD(self.CMD_PING, source, "{0}{1}".format(chr(dest),msg));
+
+    def neighborDMP(self, destination):
+        self.sendCMD(self.CMD_NEIGHBOR_DUMP, "neighbor command");
+
+    def routeDMP(self, destination):
+        self.sendCMD(self.CMD_ROUTE_DUMP, "routing command");
+
+    def addChannel(self, channelName, out=sys.stdout):
+        print 'Adding Channel', channelName;
+        self.t.addChannel(channelName, out);
+
+def main():
+    s = TestSim();
+    s.runTime(10);
+    s.loadTopo("long_line.topo");
+    s.loadNoise("no_noise.txt");
+    s.bootAll();
+    s.addChannel("command");
+    s.addChannel("general");
+
+    s.runTime(20);
+    s.ping(1, 2, "Hello, World");
+    s.runTime(10);
+    s.ping(1, 3, "Hi!");
+    s.runTime(20);
+
+if __name__ == '__main__':
+    main()
